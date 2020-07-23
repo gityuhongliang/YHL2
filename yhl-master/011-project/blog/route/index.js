32bc780f5041c095dec61d71395dbeaf51e32e1d
tom <1939185011@qq.com>
2020-07-23 23:30:36 +0800

    '7.23'

--- yhl-master/011-project/blog/route/index.js
+++ yhl-master/011-project/blog/route/index.js
@@ -2,6 +2,19 @@ const express = require('express')
 
 const route = express.Router()
 
+const CategoryModel =require('../module/category.js')
+
+async function getCommonData(){
+	//获取顶部分类数据
+	const getCategoriesDataPromise = CategoryModel.find({},'name').sort({_id:1})
+	const categoriesData  = await getCategoriesDataPromise
+	//获取点击排行数据
+	return {
+		categoriesData
+	}
+}
+ 
+//显示首页路由
 route.get('/', (req, res) => {
 	//获取cookie信息进行验证
 	/*
@@ -10,10 +23,23 @@ route.get('/', (req, res) => {
 		userInfo = JSON.parse(req.cookies.get('userInfo'))
 	}
 	*/
-	res.render('main/index',{
-		userInfo:req.userInfo
+	getCommonData()
+	.then(result=>{
+		const { categoriesData } = result
+		console.log(categoriesData)
+		res.render('main/index',{
+			userInfo:req.userInfo,
+			categoriesData
+		})
 	})
+	.catch(err=>{
+		console.log(err)
+	})
+	
 })
+
+
+//显示列表页
 route.get('/list', (req, res) => {
 	//获取cookie信息进行验证
 	/*
