ab3d3c59430f79702d541ed358f81a27f401c91b
tom <1939185011@qq.com>
2020-09-07 23:41:36 +0800

    '9.7'

--- yhl-master/017-vue-model/my-project/src/pages/home/index.vue
+++ yhl-master/017-vue-model/my-project/src/pages/home/index.vue
@@ -30,6 +30,22 @@
         square
         :icon-size="43"
         >
+        <!-- <van-grid-item>
+            <van-image src="https://api.mall.kuazhu.com/category-icons/1595243404358.jpg" />
+            <span>手机通讯</span>
+        </van-grid-item>
+        <van-grid-item>
+            <van-image src="https://api.mall.kuazhu.com/category-icons/1595243404358.jpg" />
+        </van-grid-item>
+        <van-grid-item>
+            <van-image src="https://api.mall.kuazhu.com/category-icons/1595243404358.jpg" />
+        </van-grid-item>
+        <van-grid-item>
+            <van-image src="https://api.mall.kuazhu.com/category-icons/1595243404358.jpg" />
+        </van-grid-item>
+        <van-grid-item>
+            <van-image src="https://api.mall.kuazhu.com/category-icons/1595243404358.jpg" />
+        </van-grid-item> -->
             <van-grid-item icon="https://api.mall.kuazhu.com/category-icons/1595243404358.jpg" text="手机通讯" />
             <van-grid-item icon="https://api.mall.kuazhu.com/category-icons/1595243379743.jpg" text="食品生鲜" />
             <van-grid-item icon="https://api.mall.kuazhu.com/category-icons/1595243323048.jpg" text="汽车保养" />
@@ -42,35 +58,59 @@
             <van-grid-item icon="https://api.mall.kuazhu.com/category-icons/1587973628877.png" text="电脑耗材" />
         </van-grid>
         <!-- 商品内容展示 -->
-        <van-row type="flex" justify="center">
-            <div class="Secondary-title">
-                <h2>手机通讯</h2>
-            </div>
-        </van-row>
-        <van-row class="van-top-row" type="flex" justify="center" align="center">
-            <van-col span="12">
-                <div class="content">
-                    
-                </div>
-            </van-col>
-            <van-col span="12">
-                <div class="content">
-
-                </div>
-            </van-col>
-            <van-col span="12">
-                <div class="content">
-
-                </div>
-            </van-col>
-            <van-col span="12">
-                <div class="content">
-
-                </div>
-            </van-col>
-            
-        </van-row>
-                
+        <ul class="Secondary">
+            <li>
+                <van-row type="flex" justify="center">
+                    <div class="Secondary-title">
+                        <h2>手机通讯</h2>
+                    </div>
+                </van-row>
+                <ul>
+                    <van-row 
+                        class="van-top-row"
+                        type="flex"
+                        justify="space-between"
+                        align="center"
+                        gutter="10"
+                    >
+                    <li class="Secondary-list">
+                        
+                            <div class="content">
+                                <a href="">
+                                    <img src="https://api.mall.kuazhu.com/product-images/1587973772260.jpg" alt="">
+                                    <div>
+                                        <p>
+                                             宏碁 (Acer) 极光 D606D 投影仪 投影机办公（普清 3500流明 标配HDMI 梯形校正 商用机皇新升级 白天直投）
+                                        </p>
+                                    </div>
+                                    
+                                </a>
+                            </div>
+                        
+                    </li>
+                    <li class="Secondary-list">
+                        
+                            <div class="content">
+                            </div>
+                        
+                    </li>
+                    <li class="Secondary-list">
+                        
+                            <div class="content">
+                            </div>
+                        
+                    </li>
+                    <li class="Secondary-list">
+                        
+                            <div class="content">
+                            </div>
+                        
+                    </li>
+                    </van-row>
+                </ul>
+            </li>
+        </ul>
+        
     </div>
 </template>
 <script>
@@ -119,15 +159,26 @@ export default {
         margin-top: 10px;
         font-size: 25px;
     }
-    .Secondary-title{
-        padding-bottom: 15px;
+    .Secondary{
+        display: block;
+        
+        padding-left: 5px;
+        padding-right: 5px;
+        .Secondary-list{
+            box-sizing: border-box;
+            margin-bottom: 10px;
+        }
+        .Secondary-title{
+            padding-bottom: 15px;
     
-        h2{
-            color:#323233;
-            margin-top: 10px;
-            font-size: 20px;
+            h2{
+                color:#323233;
+                margin-top: 10px;
+                font-size: 20px;
+            }
         }
     }
+    
     .van-swipe-item {
         margin-top: 14px;
         img{
@@ -138,15 +189,31 @@ export default {
     .van-top-row{
         flex-wrap: wrap;
     
-    .van-col{
-        margin-bottom: 10px;
+
         .content{
             margin:auto;
             width: 200px;
             height: 250px;
-            background-color: #ee0a24;
+            background-color: #fff;
+            img{
+                width: 100%;
+                height: 200px;
+            }
+            div{
+                width: 100%;
+               
+                p{  
+                    height: 100%;
+                    font-size: 14px;
+                    color: black;
+                    white-space:nowrap;
+                    overflow: hidden;
+                    text-overflow: ellipsis;  
+                }
+                
+            }
         }
-    }
+    
   }
 </style>
     
